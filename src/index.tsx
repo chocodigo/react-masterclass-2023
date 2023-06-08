537c43c2258d4a5f1bfdc9ea238bef2b55d89862
chocodigo <chocodigo>
2023-06-08 22:21:37 +0900

    ~8.11

--- src/index.tsx
+++ src/index.tsx
@@ -71,7 +71,7 @@ const GlobalStyle = createGlobalStyle`
   body{
     font-weight: 300;
     font-family: 'Source Sans Pro', sans-serif;
-    background:linear-gradient(135deg,#e09,#d0e);
+
     line-height: 1.2;
   }
   a{
